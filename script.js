d383a8f7625898b45ceccf314d8d1ac4d1a0afa2
Belinskyy Mykola <kalankalankov@gmail.com>
2024-01-27 11:20:38 +0100

    27.1.24

--- script.js
+++ script.js
@@ -137,10 +137,11 @@ console.log(phone);
 delete phone.weight;
 console.log(phone);
 11111111111111111111111111111111111111
-let person = {
-    name: "Alice",
-    age: 25,
-    hobbies: ['reading','cooking','traveling'],
+let student = {
+    name: `Alice`,
+    age3: 29,
+    year: 1970,
+    subjects: [`English`,`Physik`,`Mathematik`],
     address: {
         street:'123 Main Street',
         city:'New York',
@@ -149,31 +150,158 @@ let person = {
     },
 
     '1st': 1,
-    'last name': 'Smith',
+    'last name': `Smith`,
 
-    greet: function() {
-        console.log ('Hello, my name is ${this.name}');        
-    }
+    displayInfo: function() {
+    console.log(`student ${this.name} vik ${this.age3} vuvchaestudent ${this.subjects}`);        
+    },
+    calculateAge: function (year_now) {
+        return year_now - this.year;
+    },
 };
 
-console.log(person.name);
-console.log(person['name']);
+student.displayInfo();
+console.log(student.name);
+console.log(student['name']);
 
-console.log(person['1st']);
-console.log(person['last name']);
+console.log(student['1st']);
+console.log(student['last name']);
 
 let key = 'address';
-console.log(person[key]);
+console.log(student[key]);
 
-console.log(person.address.state);
-console.log(person['address']['city']);
+console.log(student.address.state);
+console.log(student['address']['city']);
 
-person.greet();
+
+
+let age2 = student.calculateAge(3044);
+console.log(age2);
+
+for (let key in student) {
+    console.log(key + ': ' + student[key]);
+}
+
 11111111111111111111111111111111111111
 
-//console person = {
-   // name:'John',
-   // year: 1970,
-   // occupation:'Software Engineer',
-    
-//};
+let globalVar = `I am a global varieble`;
+function myFunc() {
+    console.log(globalVar);
+
+}
+myFunc();
+console.log(globalVar);
+1111111111111111111111111111111111111
+
+function myFunc2() {
+    let localVar = `I am a local varieble`;
+    console.log(localVar);
+
+}
+myFunc2();
+//console.log(localVar);
+111111111111111111111111111111111111111
+
+//SPRET OPERETOR
+let number12 = [1,2,3];
+console.log(...number12);
+console.log(number12);
+ 
+
+//pruklad
+let fruits3 = ["aple", "banana", "orange"];
+let fruitsCopy = [...fruits3];
+fruitsCopy.push("pear");
+console.log (fruits3);
+console.log (fruitsCopy);
+
+//pryklad2
+let cars2 = [{model: `Tesla`, color2: `read`}, {model: `Toyota`, color2: `blue`}];
+let cars2Copy = [...cars2];
+cars2Copy[0].color2 = 'black';
+console.log(cars2);
+console.log(cars2Copy);
+
+//pryklad3
+let colors4 = [`red`, `green`,`blue`];
+let shapes = [`circle`, `squer`, `triangle`];
+let colors4AndShapes = [...colors4, ...shapes];
+console.log(colors4AndShapes);
+
+//pryklad4
+let numbersw = [1, 2, 3, 4, 5];
+let newNumbers1 = [...numbersw.slice(0,2),6, 7, ...numbersw.slice(2)];
+let newNumbers2 = [...numbersw, 6];
+console.log (newNumbers1);
+console.log (newNumbers2);
+
+
+//FUNKTION EROW ES6
+const square2 = (num1) => {
+    return num1 * num1;
+}
+
+const square3 = num => num * num;
+console.log(square2);
+
+let multiply = (a, b) => a*b;
+//DDDDDDDDDDDDDDZZZZZZZZZZZZZZZ
+const squareArray = (numbers) => numbers.map(number => number ** 2);
+
+const inputArray = [1, 2, 3, 4, 5];
+const squaredNumbers = squareArray(inputArray);
+
+console.log(squaredNumbers);
+111111111111111111111111111111111111
+
+//LITRALY
+let x = 5;
+let y = 10;
+let sum = `The sum of ${x} and ${y} is ${x + y}.`;
+console.log(sum);
+
+//pryklad2
+let age1 = 24;
+let birthday = `Happy ${age1}th birthday!`;
+console.log(birthday);
+
+//pryklad3
+let person = {
+    namee: `Bob`,
+    agee: 38,
+    occupation: `teacher`
+};
+let introduction = `My name is ${person.namee}, I am ${person.agee} years old,
+and I work as a ${person.occupation}.`;
+console.log(introduction);
+
+//pryklad4
+let isEven = n => n % 2 === 0;
+let numberp = 42;
+let parity = `The number ${numberp} is ${isEven(numberp) ? `even` : `odd`}.`;
+console.log(parity);
+
+//pryklad5
+const message = `Escape a backtick: \` inside template literal.`;
+console.log(message);
+
+//pryklad56   //z literalamy
+let poem = `Do not go gentle into that good night,
+Old age should burn and rave at close of day;
+Rage, rage against the dying of the light.`;
+console.log(poem);
+//bez literaliv
+let poem2 = "Do not go gentle into that good night, \n" +
+"Old age should burn and rave at close of day; \n" + 
+"Rage, rage against the dying of the light.\n";
+console.log(poem2);
+
+//DDDDDDDDDDDDDDDDDDDDDDDDDZZZZZZZZZZZZZZZZZZZZZZZZZ
+let student1 = {
+    namee: `Vova`,
+    agee: 38,
+    occupation: `teacher`,
+};
+let introduction1 = `My name is ${student1.namee}, I am ${student1.agee} years old,
+and I stading a ${student1.occupation}.`;
+console.log(introduction1);
